5bff0308dd1e986a8078fed37fa879a196f52bb6
dannnnnno-o <dannnnnnnno.o@gmail.com>
2026-07-15 16:14:46 +0800

    misc functions for Act2

--- misc/Misc.java
+++ misc/Misc.java
@@ -1,4 +1,5 @@
 package misc;
+import act2.*;
 public class Misc {
     public void Clear(){
         System.out.print("\033[H\033[2J");
@@ -19,6 +20,11 @@ public class Misc {
         System.out.println();
     }
 
+    public String OptionRange(int floor, int ceiling){
+        String option = "["+ Integer.toString(floor) +" - " + Integer.toString(ceiling) +"]";
+        return option;        
+    }
+
     public String OptionRange(String[] x, boolean isIndex){
         String option;
         if(isIndex){
@@ -32,6 +38,24 @@ public class Misc {
         return option;
     }
     
+    public String OptionRange(Item[] items){
+        int item_count = 0;
+        String range;
+        for(Item item : items){
+            item_count++;
+        }
+        if(item_count == 0){
+            range = "[0]";
+        }
+        else if(item_count == 1){
+            range = "[1]";
+        }
+        else{
+            range = "[1 - " + Integer.toString(item_count) + "]";
+        }
+
+        return range;
+    }
     public void Options(String[] x){
         for(int i = 0; i < x.length; i++){
             System.out.println("[" + (i + 1) + "] " + x[i]);
